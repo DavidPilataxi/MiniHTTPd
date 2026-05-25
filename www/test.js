0f55a448b086b271900c2b5b967befed32cf0e0c
DavidPilataxi <david.pilataxi@epn.edu.ec>
2026-05-25 12:16:05 -0500

    Delete www directory

--- www/test.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const title = document.querySelector("h1");
-
-    title.textContent += " + JavaScript";
-
-    console.log("JavaScript loaded correctly");
-});
